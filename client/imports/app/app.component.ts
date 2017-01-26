99b9b06df22c0136e49e89807e050faa1fe34544
Alex <alex.borodach@gmail.com>
2017-01-26 23:45:27 +0700

    fix: show chats page for logged-in user

--- client/imports/app/app.component.ts
+++ client/imports/app/app.component.ts
@@ -1,4 +1,4 @@
-import {Component} from '@angular/core';
+import {NgZone, Component} from '@angular/core';
 import {Platform} from 'ionic-angular';
 import {StatusBar} from 'ionic-native';
 import {Meteor} from 'meteor/meteor';
@@ -14,8 +14,11 @@ import {LoginComponent} from '../pages/auth/login.component';
 export class AppComponent {
   rootPage: any;
 
-  constructor(platform: Platform) {
-    this.rootPage = Meteor.user() ? TabsContainerComponent : LoginComponent;
+  constructor(platform: Platform, private zone: NgZone) {
+    Tracker.autorun(() => zone.run(() => { 
+      this.rootPage = Meteor.user() ? TabsContainerComponent : LoginComponent;
+    }));
+
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
